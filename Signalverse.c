6d80d8033678f9d092cd6a9eae09cbca70362228
zgeorgin <zgeorginn@gmail.com>
2023-10-24 09:35:16 +0300

    Added Fourier!
    Added forward and reverse Fourier transform!

--- Signalverse.c
+++ Signalverse.c
@@ -9,7 +9,15 @@ double SV_LINE(double x)
 	return x;
 }
 
-typedef struct SV_Signal
+double SV_MEANDER(double x)
+{
+	if (x < 0)
+		return 0;
+	if (x < 0.5)
+		return 1;
+	return 0;
+}
+typedef struct 
 {
 	double* amps;
 	double period;
@@ -17,7 +25,7 @@ typedef struct SV_Signal
 } SV_Signal;
 
 
-typedef struct SV_Complex
+typedef struct
 {
 	double real;
 	double imag;
@@ -100,7 +108,7 @@ SV_Signal SV_MULT_DOUBLE_SIGNAL(double k, SV_Signal s)
 	return res;
 }
 
-#define SV_MULT(X, Y) _Generic((X),\
+#define SV_Mult(X, Y) _Generic((X),\
     SV_Complex:   _Generic((Y),\
         SV_Complex:   SV_MULT_COMPLEX_COMPLEX,\
         double: SV_MULT_COMPLEX_DOUBLE\
@@ -109,6 +117,17 @@ SV_Signal SV_MULT_DOUBLE_SIGNAL(double k, SV_Signal s)
         double:   SV_MULT_SIGNAL_DOUBLE\
     )\
 )(X, Y)
+
+SV_Complex SV_Sum(SV_Complex z1, SV_Complex z2)
+{
+	SV_Complex res = SV_create_complex(z1.real + z2.real, z1.imag + z2.imag);
+	return res;
+}
+
+double SV_Abs(SV_Complex z1)
+{
+	return z1.real * z1.real + z1.imag * z1.imag;
+}
 
 SV_Signal SV_expand_signal(SV_Signal s, double time)
 {
@@ -183,14 +202,53 @@ SV_Signal SV_triangle_signal(double dt)
 	return res;
 }
 
-/*SV_Complex* SV_Forward_Fourier_transform (SV_Signal s)
+SV_Signal SV_meander_signal(double dt)
 {
-	SV_Complex a;
-	return a;
-}*/
+	SV_Signal res = SV_create_signal_from_function(SV_MEANDER, 1, dt);
+	return res;
+}
+
+SV_Complex SV_exp (SV_Complex z)
+{
+	SV_Complex res = SV_create_complex(exp(z.real) * cos(z.imag), exp(z.real) * sin(z.imag));
+	return res;
+}
+
+SV_Complex* SV_forward_Fourier_transform (SV_Signal s)
+{
+	int size = (int)(s.period/s.dt);
+	SV_Complex* res = (SV_Complex*) malloc(size * sizeof(SV_Complex));
+	for (int i = 0; i < size; i++)
+	{
+		SV_Complex sum = SV_create_complex(0.0, 0.0);
+		for (int j = 0; j < size; j++)
+		{
+			sum = SV_Sum(sum, SV_Mult(SV_exp(SV_create_complex(0.0, -2*PI*j*i / size)), s.amps[j]));
+		}
+		res[i] = sum;
+ 	}
+	return res;
+}
+
+SV_Signal SV_reverse_Fourier_transform (SV_Complex* F, size_t size, double period)
+{
+	SV_Signal res;
+	res.amps = (double*) malloc (size * sizeof(double));
+	for (int i = 0; i < size; i++)
+	{
+		double sum = 0;
+		for (int j = 0; j < size; j++)
+		{
+			res.amps[i] = 1.0/size * SV_MULT_COMPLEX_COMPLEX(F[j], SV_exp(SV_create_complex(0, 2*PI/size * i * j))).real;
+		}
+	}
+	res.dt = period/size;
+	res.period = period;
+	return res;
+}
 /* 
 	TODO:
 	1.) Error handling
-	2.) Furier_transform
+	2.) Signal visualisation
 	3.) A lot more...
 */ 
--- example.c
+++ example.c
@@ -2,7 +2,14 @@
 
 int main()
 {
-    SV_Signal example = SV_sin_signal(0.001);
-    SV_save_signal_to_file(SV_MULT(SV_expand_signal(example, 4*PI), 10.0), "./test.txt");
+   /* SV_Signal example = SV_meander_signal(0.01);
+    SV_save_signal_to_file(SV_Mult(SV_expand_signal(example, 4*PI), 10.0), "./test.txt");*/
+
+    SV_Signal example = SV_sin_signal(0.01);
+    SV_Complex* F = SV_forward_Fourier_transform(example);
+    size_t size = (size_t)(example.period/example.dt);
+    SV_Signal transformed_example = SV_reverse_Fourier_transform(F, size, 2*PI);
+    SV_save_signal_to_file(transformed_example, "./test_Fourier.txt");
+    SV_save_signal_to_file(example, "./test_sin.txt");
     return 0;
 }
